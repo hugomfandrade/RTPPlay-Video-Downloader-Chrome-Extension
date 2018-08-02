62fbc00a7890386cf4d44e8042aeaec894acf4de
Hugo Andrade <hugo.mf.andrade@gmail.com>
2018-08-02 09:25:09 +0100

    Code refactoring for ease to scale up to another platforms (SIC, multiple RTPPlay videos, ...)

--- fetchLinks.js
+++ fetchLinks.js
@@ -4,13 +4,14 @@
 
 'use strict';
 
-/*String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};/**/
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+String.prototype.indexOfEx = function(text) {
+    var target = this;
+    return target.indexOf(text) + text.length;
 };
 
 function getTabTitle() {
@@ -18,7 +19,79 @@ function getTabTitle() {
         .replaceAll('-',' ')
         .replace(/\s{2,}/g,' ')
         .replaceAll(' ', '.')
-        .normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+        .normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+}
+
+function isValid(scriptTags) {
+    
+    if (scriptTags == undefined) return false;
+    
+    var text = scriptTags.text;
+    
+    if (text.length == 0 || text.indexOf('RTPPlayer') < 0) return false;
+    
+    return true;
+}
+
+function isValidAndGetType(scriptTags, callback) {
+    
+    if (scriptTags == undefined) {
+        return callback(false);
+    }
+    
+    var text = scriptTags.text;
+    
+    if (text.length == 0) {
+        return callback(false);
+    }
+    
+    if (text.indexOf('RTPPlayer') >= 0) {
+        return callback(true, 'RTPPlay');
+    }
+
+    return callback(false);
+}
+
+function download(link, filename) {
+    
+    //console.log('filename = ' + filename);
+    //console.log('linkSubString (audio) = ' + linkSubString);
+    chrome.runtime.sendMessage({linkSubString: link, filename: filename}, function(response) { });
+}
+
+function downloadRTPPlay(scriptText) {
+    
+
+    var rtpPlayerSubString = scriptText.substring(scriptText.indexOfEx('RTPPlayer({') , scriptText.lastIndexOf('})'));
+
+    if (rtpPlayerSubString.indexOf('file: \"') >= 0) {
+
+        var link = rtpPlayerSubString.substr(
+            rtpPlayerSubString.indexOfEx('file: \"'), 
+            rtpPlayerSubString.substr(rtpPlayerSubString.indexOfEx('file: \"')).indexOf('\",'));
+
+        if (link.indexOf('.mp4') >= 0) { // is video file
+
+            link = 
+                link.substring(0, link.indexOf('index')) +
+                link.substring(link.indexOfEx('streams='), link.indexOfEx('.mp4'));
+
+            var filename = getTabTitle() + ".mp4";
+
+            download(link, filename);
+
+            return true;
+        }
+        else if (link.indexOf('.mp3') >= 0) { // is audio file
+
+            var filename = getTabTitle() + ".mp3";
+
+            download(link, filename);
+
+            return true;
+        }
+    }
+    return false;
 }
 
 var scriptTags = document.getElementsByTagName('script');
@@ -27,63 +100,17 @@ var isFound = false;
 
 for (var i = 0 ; i < scriptTags.length ; i++) {
     
-    if (scriptTags[i] != undefined) {
-        var text = scriptTags[i].text;
-
-        if (text.length > 0) {
-
-            if (text.indexOf('RTPPlayer') >= 0) {
-                
-                var rtpPlayerSubString = text.substring(
-                    text.indexOf('RTPPlayer({') + 'RTPPlayer({'.length, 
-                    text.lastIndexOf('})')
-                );
-                
-                if (rtpPlayerSubString.indexOf('file: \"') >= 0) {
-
-                    var link = rtpPlayerSubString.substr(
-                        rtpPlayerSubString.indexOf('file: \"') + 'file: \"'.length, 
-                        rtpPlayerSubString.substr(rtpPlayerSubString.indexOf('file: \"') + 'file: \"'.length).indexOf('\",')
-                    );
-                    
-                    if (link.indexOf('.mp4') >= 0) { // is video file
-
-                        var linkSubString = 
-                            link.substring(
-                                0, 
-                                link.indexOf('index')
-                            ) +
-                            link.substring(
-                                link.indexOf('streams=') + 'streams='.length, 
-                                link.indexOf('.mp4') + '.mp4'.length
-                            )
-                        ;
-                        
-                        var filename = getTabTitle() + ".mp4";
-                        
-                        //console.log('filename = ' + filename);
-                        //console.log('linkSubString (video) = ' + linkSubString);
-                        
-                        chrome.runtime.sendMessage({linkSubString: linkSubString, filename: filename}, function(response) { });
-
-                        isFound = true;
-                    }
-                    else if (link.indexOf('.mp3') >= 0) { // is audio file
-                        
-                        var filename = getTabTitle() + ".mp3";
-                        
-                        //console.log('filename = ' + filename);
-                        //console.log('linkSubString (audio) = ' + linkSubString);
-                        chrome.runtime.sendMessage({linkSubString: link, filename: filename}, function(response) { });
-
-                        isFound = true;
-                    }
-                }
+    isValidAndGetType(scriptTags[i], function(isOk, type) {
+        
+        if (isOk) {
+            
+            if (type == 'RTPPlay') {
+                isFound = downloadRTPPlay(scriptTags[i].text);
             }
         }
-    }
+    });
 }
    
 if (isFound == false) {
-    alert('No RTPPlayer file not found');
+    alert('No RTPPlayer file found');
 }
--- isValid.js
+++ isValid.js
@@ -4,54 +4,79 @@
 
 'use strict';
 
+String.prototype.indexOfEx = function(text) {
+    var target = this;
+    return target.indexOf(text) + text.length;
+};
+
+function isValid(scriptTags, callback) {
+    
+    if (scriptTags == undefined) return false;
+    
+    var text = scriptTags.text;
+    
+    if (text.length == 0 || text.indexOf('RTPPlayer') < 0) return false;
+    
+    return true;
+}
+
+function isValidAndGetType(scriptTags, callback) {
+    
+    if (scriptTags == undefined) {
+        return callback(false);
+    }
+    
+    var text = scriptTags.text;
+    
+    if (text.length == 0) {
+        return callback(false);
+    }
+    
+    if (text.indexOf('RTPPlayer') >= 0) {
+        return callback(true, 'RTPPlay');
+    }
+
+    return callback(false);
+}
+
+function isValidRTPPlay(scriptText) {
+    
+    var rtpPlayerSubString = scriptText.substring(scriptText.indexOfEx('RTPPlayer({') , scriptText.lastIndexOf('})'));
+
+    if (rtpPlayerSubString.indexOf('file: \"') >= 0) {
+
+        var link = rtpPlayerSubString.substr(
+            rtpPlayerSubString.indexOfEx('file: \"'), 
+            rtpPlayerSubString.substr(rtpPlayerSubString.indexOfEx('file: \"')).indexOf('\",'));
+
+        if (link.indexOf('.mp4') >= 0) { // is video file
+
+            return true;
+        }
+        else if (link.indexOf('.mp3') >= 0) { // is audio file
+
+            return true;
+        }
+    }
+    
+    return false;
+}
+
 var scriptTags = document.getElementsByTagName('script');
 
 var isFound = false;
 
 for (var i = 0 ; i < scriptTags.length ; i++) {
     
-    if (scriptTags[i] != undefined) {
-        var text = scriptTags[i].text;
-
-        if (text.length > 0) {
-
-            if (text.indexOf('RTPPlayer') >= 0) {
-                
-                var rtpPlayerSubString = text.substring(
-                    text.indexOf('RTPPlayer({') + 'RTPPlayer({'.length, 
-                    text.lastIndexOf('})')
-                );
-                
-                if (rtpPlayerSubString.indexOf('file: \"') >= 0) {
-
-                    var link = rtpPlayerSubString.substr(
-                        rtpPlayerSubString.indexOf('file: \"') + 'file: \"'.length, 
-                        rtpPlayerSubString.substr(rtpPlayerSubString.indexOf('file: \"') + 'file: \"'.length).indexOf('\",')
-                    );
-                    
-                    if (link.indexOf('.mp4') >= 0) { // is video file
-
-                        var linkSubString = 
-                            link.substring(
-                                0, 
-                                link.indexOf('index')
-                            ) +
-                            link.substring(
-                                link.indexOf('streams=') + 'streams='.length, 
-                                link.indexOf('.mp4') + '.mp4'.length
-                            )
-                        ;
-
-                        isFound = true;
-                    }
-                    else if (link.indexOf('.mp3') >= 0) { // is audio file
-
-                        isFound = true;
-                    }
-                }
+    isValidAndGetType(scriptTags[i], function(isOk, type) {
+        
+        if (isOk) {
+            
+            if (type == 'RTPPlay') {
+                isFound = isValidRTPPlay(scriptTags[i].text);
             }
         }
-    }
+    });
 }
 
 chrome.runtime.sendMessage({MessageType: 'isValid', isValid: isFound}, function(response) {});
