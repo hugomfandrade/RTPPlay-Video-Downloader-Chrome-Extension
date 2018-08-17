f673cc8240b2461db487a29e7ff5a6106260b3b8
Hugo Andrade <hugo.mf.andrade@gmail.com>
2018-08-17 16:35:17 +0100

    refactoring

--- isValid.js
+++ isValid.js
@@ -9,60 +9,6 @@ String.prototype.indexOfEx = function(text) {
     return target.indexOf(text) + text.length;
 };
 
-function isValid(scriptTags, callback) {
-    
-    if (scriptTags == undefined) return false;
-    
-    var text = scriptTags.text;
-    
-    if (text.length == 0 || text.indexOf('RTPPlayer') < 0) return false;
-    
-    return true;
-}
-
-function isValidAndGetType(scriptTags, callback) {
-    
-    if (scriptTags == undefined) {
-        return callback(false);
-    }
-    
-    var text = scriptTags.text;
-    
-    if (text.length == 0) {
-        return callback(false);
-    }
-    
-    if (text.indexOf('RTPPlayer') >= 0) {
-        return callback(true, 'RTPPlay');
-    }
-
-    return callback(false);
-}
-
-function isValidRTPPlay(scriptText) {
-    
-    
-    var rtpPlayerSubString = scriptText.substring(scriptText.indexOfEx('RTPPlayer({') , scriptText.lastIndexOf('})'));
-
-    if (rtpPlayerSubString.indexOf('file: \"') >= 0) {
-
-        var link = rtpPlayerSubString.substr(
-            rtpPlayerSubString.indexOfEx('file: \"'), 
-            rtpPlayerSubString.substr(rtpPlayerSubString.indexOfEx('file: \"')).indexOf('\",'));
-
-        if (link.indexOf('.mp4') >= 0) { // is video file
-
-            return true;
-        }
-        else if (link.indexOf('.mp3') >= 0) { // is audio file
-
-            return true;
-        }
-    }
-    
-    return false;
-}
-
 function getRTPPlayLinkFromScript(scriptText) {
     if (scriptText === undefined || 
         scriptText.length === 0 || 
@@ -114,7 +60,7 @@ function getType() {
     return "unknown";
 }
 
-function newIsValid() {
+function isValid() {
     
     var type = getType();
     
@@ -144,37 +90,7 @@ function newIsValid() {
     return false;
 }
 
-function newIsValidAndGetType(callback) {
-    
-    if (window.location.href.indexOf("www.rtp.pt/play") >= 0) {
-        // might be an RTPPlay file
-    
-        if (document === undefined) {
-            return callback(false);
-        }
-        
-        var scriptTags = document.getElementsByTagName('script');
-        
-        if (scriptTags === undefined) {
-            return callback(false);
-        }
-        
-        for (var i = 0 ; i < scriptTags.length ; i++) {
-            
-            var text = scriptTags[i].text;
-
-            if (text.length !== 0
-                && text.indexOf('RTPPlayer') >= 0
-                && isValidRTPPlay(text) === true) {
-                return callback(true, 'RTPPlay');
-            }
-        }
-    }
-
-    return callback(false);
-}
-
-var isOk = newIsValid();
+var isOk = isValid();
 var type = getType();
 
 chrome.runtime.sendMessage({MessageType: 'isValid', isValid: isOk, type: type}, function(response) {});
