640855256708db649befa9bbe8161f5d19be2560
Hugo Andrade <hugo.mf.andrade@gmail.com>
2019-08-31 23:24:11 +0100

    browser action icon changes according to the type (rtp, sic, ...)

--- background.js
+++ background.js
@@ -3,16 +3,26 @@ var activeWindowID;
 
 chrome.contextMenus.removeAll();
 chrome.contextMenus.create({
-    id: "disable_context_menu_item",
+    id: "download_context_menu_item",
+    title: "Download",
+    contexts: ["browser_action"]
+});
+chrome.contextMenus.create({
+    id: "download_all_context_menu_item",
     title: "Download all",
     contexts: ["browser_action"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "disable_context_menu_item") {
+    if (info.menuItemId === "download_all_context_menu_item") {
         chrome.tabs.executeScript(tab.id, {file: "backgroundDatatype.js"});
         chrome.tabs.executeScript(tab.id, {file: "fetchLinksLib.js"});
         chrome.tabs.executeScript(tab.id, {file: "fetchAllLinks.js"});
+    }
+    else if (info.menuItemId === "download_context_menu_item") {
+        chrome.tabs.executeScript(tab.id, {file: "backgroundDatatype.js"});
+        chrome.tabs.executeScript(tab.id, {file: "fetchLinksLib.js"});
+        chrome.tabs.executeScript(tab.id, {file: "fetchLinks.js"});
     }
 });
     
@@ -23,25 +33,75 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    chrome.tabs.executeScript(null, {
-        code: 'var debugmessage = ' + '"onMessage = ' + request.isValid + ' , ' + request.isEpisodeValid + '"' + ';'
-    }, function() {chrome.tabs.executeScript(null, {file: 'debugMessage.js'});});
+    
     if (request.MessageType === 'isValid') {
+    
+        chrome.tabs.executeScript(null, {
+            code: 'var debugmessage = ' + '"onMessage = ' + request.isValid + ' , ' + request.isEpisodeValid + '"' + ';'
+        }, function() {chrome.tabs.executeScript(null, {file: 'debugMessage.js'});});
+
         if (sender.tab.id === activeTabID) {
+            
             if (request.isValid === true) {
-                chrome.browserAction.enable(null, function() {  });
+                chrome.browserAction.enable(activeTabID, function() {  });
             }
             else {
-                chrome.browserAction.disable(null, function() { });
+                chrome.browserAction.disable(activeTabID, function() { });
             }
             
-            chrome.contextMenus.update("disable_context_menu_item", {enabled: request.isEpisodeValid});
+            chrome.contextMenus.update("download_context_menu_item", {enabled: request.isValid});
+            chrome.contextMenus.update("download_all_context_menu_item", {enabled: request.isEpisodeValid});
+            
+            setBrowserActionIcon(request.type)
         }
     }
     else if (request.MessageType === 'download') {
         chrome.downloads.download({url: request.link, filename: request.filename},function(id) {});
     }
 });
+
+function setBrowserActionIcon(type) {
+    if (type === undefined || type === "RTPPlay") {
+        chrome.browserAction.setIcon({path: {
+            "16": "images/rtpplay_downloader_16.png",
+            "32": "images/rtpplay_downloader_32.png",
+            "48": "images/rtpplay_downloader_48.png",
+            "128": "images/rtpplay_downloader_128.png"
+        }});
+    }
+    else if (type === "SICRadical") {
+        chrome.browserAction.setIcon({path: {
+            "16": "images/sicradical_downloader_16.png",
+            "32": "images/sicradical_downloader_32.png",
+            "48": "images/sicradical_downloader_48.png",
+            "128": "images/sicradical_downloader_128.png"
+        }});
+    }
+    else if (type === "SICNoticias") {
+        chrome.browserAction.setIcon({path: {
+            "16": "images/sicnoticias_downloader_16.png",
+            "32": "images/sicnoticias_downloader_32.png",
+            "48": "images/sicnoticias_downloader_48.png",
+            "128": "images/sicnoticias_downloader_128.png"
+        }});
+    }
+    else if (type === "SIC") {
+        chrome.browserAction.setIcon({path: {
+            "16": "images/sic_downloader_16.png",
+            "32": "images/sic_downloader_32.png",
+            "48": "images/sic_downloader_48.png",
+            "128": "images/sic_downloader_128.png"
+        }});
+    }
+    else {
+        chrome.browserAction.setIcon({path: {
+            "16": "images/rtpplay_downloader_16.png",
+            "32": "images/rtpplay_downloader_32.png",
+            "48": "images/rtpplay_downloader_48.png",
+            "128": "images/rtpplay_downloader_128.png"
+        }});
+    }
+}
 
 chrome.runtime.onInstalled.addListener(function() {
     
